b749ca4df21b98becd6308c9e3d694e260501d7d
Caglar Gonul <caglar.gonul.001@gmail.com>
2019-11-14 10:16:29 +0300

    Expression evaluator construct.

--- antlr-basics/src/main/java/com/poc/chapter_07_part02/impl/ExpressionEvaluaterListener.java
+++ antlr-basics/src/main/java/com/poc/chapter_07_part02/impl/ExpressionEvaluaterListener.java
@@ -5,11 +5,29 @@ import com.poc.chapter_07_part02.grammar.ExprParser;
 
 public class ExpressionEvaluaterListener extends ExprBaseListener {
 
-	private Integer value = 0;
+    private Integer value = 0;
 
-	@Override
-	public void enterAdd(ExprParser.AddContext ctx) {
+    @Override
+    public void enterAdd(ExprParser.AddContext ctx) {
+        String left = ctx.e().get(0).getText();
+        String right = ctx.e().get(3).getText();
 
-	}
+        Integer leftInt = Integer.parseInt(left);
+        Integer rightInt = Integer.parseInt(right);
+        value += (leftInt + rightInt);
+    }
 
+    @Override
+    public void enterMult(ExprParser.MultContext ctx) {
+        String left = ctx.e().get(0).getText();
+        String right = ctx.e().get(3).getText();
+
+        Integer leftInt = Integer.parseInt(left);
+        Integer rightInt = Integer.parseInt(right);
+        value += (leftInt * rightInt);
+    }
+
+    public Integer getValue() {
+        return value;
+    }
 }
--- antlr-basics/src/main/java/com/poc/chapter_07_part02/impl/Test01.java
+++ antlr-basics/src/main/java/com/poc/chapter_07_part02/impl/Test01.java
@@ -1,8 +1,23 @@
 package com.poc.chapter_07_part02.impl;
 
+import com.poc.chapter_07_part02.grammar.ExprLexer;
+import com.poc.chapter_07_part02.grammar.ExprParser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
 public class Test01 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        ExprLexer exprLexer = new ExprLexer(CharStreams.fromString("1+3*5+7"));
+        CommonTokenStream commonTokenStream = new CommonTokenStream(exprLexer);
+        ExprParser exprParser = new ExprParser(commonTokenStream);
+        ParseTree parseTree = exprParser.s();
 
-	}
+        ParseTreeWalker parseTreeWalker = new ParseTreeWalker();
+        ExpressionEvaluaterListener expressionEvaluaterListener = new ExpressionEvaluaterListener();
+        parseTreeWalker.walk(expressionEvaluaterListener, parseTree);
+        System.out.println("result = " + expressionEvaluaterListener.getValue());
+    }
 }
